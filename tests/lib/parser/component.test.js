1fa6bd2b5fae8214b503565fec5b57687eaf385c
Tom <hzlhu.dargon@gmail.com>
2013-08-21 21:44:43 +0800

    complete the config & component parser and the tests

---
+++ tests/lib/parser/component.test.js
@@ -0,0 +1,91 @@
+var mocha = require('mocha'),
+    should = require('should'),
+    Component = require('../../../lib/parser/component');
+
+describe('Component(__dirname + "/../../fixtures/components.json")', function() {
+    var components = {
+            "jquery": {
+                "javascript": ["jquery/jquery.js"],
+                "css": []
+            },
+
+            "jquery_ui_core": {
+                "javascript": [
+                    "jquery/jquery.js",
+                    "jquery/ui/jquery.ui.core.js"
+                ],
+                "css": [
+                    "jquery/ui/themes/base/jquery.ui.core.css",
+                    "jquery/ui/themes/base/jquery.ui.theme.css"
+                ]
+            },
+
+            "jquery_ui_mouse": {
+                "javascript": [
+                    "jquery/jquery.js",
+                    "jquery/ui/jquery.ui.core.js",
+                    "jquery/ui/jquery.ui.widget.js",
+                    "jquery/ui/jquery.ui.mouse.js"
+                ],
+                "css": [
+                    "jquery/ui/themes/base/jquery.ui.core.css",
+                    "jquery/ui/themes/base/jquery.ui.theme.css",
+                    "jquery/ui/themes/base/jquery.ui.widget.css",
+                    "jquery/ui/themes/base/jquery.ui.mouse.css"
+                ]
+            },
+
+            "jquery_ui_button": {
+                "javascript": [
+                    "jquery/jquery.js",
+                    "jquery/ui/jquery.ui.core.js",
+                    "jquery/ui/jquery.ui.widget.js",
+                    "jquery/ui/jquery.ui.mouse.js",
+                    "jquery/ui/jquery.ui.button.js"
+                ],
+                "css": [
+                    "jquery/ui/themes/base/jquery.ui.core.css",
+                    "jquery/ui/themes/base/jquery.ui.theme.css",
+                    "jquery/ui/themes/base/jquery.ui.widget.css",
+                    "jquery/ui/themes/base/jquery.ui.mouse.css",
+                    "jquery/ui/themes/base/jquery.ui.button.css"
+                ]
+            },
+
+            "jquery_ui_dialog": {
+                "javascript": [
+                    "jquery/jquery.js",
+                    "jquery/ui/jquery.ui.core.js",
+                    "jquery/ui/jquery.ui.widget.js",
+                    "jquery/ui/jquery.ui.mouse.js",
+                    "jquery/ui/jquery.ui.button.js",
+                    "jquery/ui/jquery.ui.draggable.js",
+                    "jquery/ui/jquery.ui.position.js",
+                    "jquery/ui/jquery.ui.dialog.js"
+                ],
+                "css": [
+                    "jquery/ui/themes/base/jquery.ui.core.css",
+                    "jquery/ui/themes/base/jquery.ui.theme.css",
+                    "jquery/ui/themes/base/jquery.ui.widget.css",
+                    "jquery/ui/themes/base/jquery.ui.mouse.css",
+                    "jquery/ui/themes/base/jquery.ui.button.css",
+                    "jquery/ui/themes/base/jquery.ui.draggable.css",
+                    "jquery/ui/themes/base/jquery.ui.position.css",
+                    "jquery/ui/themes/base/jquery.ui.dialog.css"
+                ]
+            }
+        },
+        component = new Component(__dirname + '/../../fixtures/components.json');
+
+    describe('#parse()', function() {
+        it('should equality', function() {
+            component.parse().should.eql(components);
+        });
+    });
+
+    describe('#parse("jquery_ui_dialog")', function() {
+        it('should equality', function() {
+            component.parse('jquery_ui_dialog').should.eql(components['jquery_ui_dialog']);
+        });
+    });
+});
---
+++ tests/lib/parser/config.test.js
@@ -0,0 +1,32 @@
+var mocha = require('mocha'),
+    should = require('should'),
+    Config = require('../../../lib/parser/config');
+
+describe('Config(__dirname + "/../../config.json")', function() {
+    var configs = {
+            "path": {
+                "components" : "fixtures/components.json"
+            }
+        },
+        config = new Config(__dirname + '/../../config.json');
+
+    config.read();
+
+    describe('#read()', function() {
+        it ('config.configs should equality', function() {
+            config.configs.should.eql(configs);
+        });
+    });
+
+    describe('#get("path")', function() {
+        it ('should be equality', function() {
+            config.get('path').should.eql(configs['path']);
+        });
+    });
+
+    describe('#get("path", "components")', function() {
+        it('should be equal ', function() {
+            config.get('path', 'components').should.equal(configs['path']['components']);
+        })
+    });
+});
