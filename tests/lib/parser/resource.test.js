81d27cc4287c20dc43948df875841022451573e5
Tom <hzlhu.dargon@gmail.com>
2013-08-23 16:35:03 +0800

    refactor resource parser logic & fixed the test

--- tests/lib/parser/resource.test.js
+++ tests/lib/parser/resource.test.js
@@ -6,35 +6,6 @@ var fs = require('fs'),
 describe('Resource(__dirname + "/../../.rbuildrc")', function() {
     var resource = new Resource(__dirname + '/../../.rbuildrc');
 
-    describe('#getConfigs()', function() {
-        var configs = {
-            "home@index": {
-                "javascript": [
-                    "res:jquery",
-                    "home.js"
-                ],
-                "css": ["base.css"],
-                "dest_prefix": "home-min"
-            },
-            "users@index": {
-                "javascript": [
-                    "res:jquery",
-                    "res:jquery_ui_dialog",
-                    "users.js"
-                ],
-                "css": [
-                    "base.css",
-                    "res:jquery_ui_dialog"
-                ],
-                "dest_prefix": "users-min"
-            }
-        };
-
-        it('should equality', function() {
-            resource.getConfigs().should.eql(configs);
-        });
-    });
-
     describe('#parse()', function() {
         var resources = {
             "controllers/home@index": {
@@ -82,21 +53,24 @@ describe('Resource(__dirname + "/../../.rbuildrc")', function() {
     });
 
     describe('#save()', function() {
-        it('should equality', function() {
-            var resources = {
-                "controllers/home@index": {
-                    "javascript": "home-min_1b6316803455be0c0bdbe08fd52c44b5.js",
-                    "css": "home-min_15a63a0c6a738eb8de8e823d2b5aafda.css"
-                },
-                "controllers/users@index": {
-                    "javascript": "users-min_f9681fdf76cf085a6c5b2295112398de.js",
-                    "css": "users-min_48d508549dffd696499a5173ef898c13.css"
-                }
-            };
-            resource.save();
-            JSON.parse(
-                fs.readFileSync(__dirname + '/../../rbuild.lock'
-            ).toString()).should.eql(resources);
+        resource.save();
+
+        describe('Read file __dirname + "/../../rbuild.lock"', function() {
+            it('should equality', function() {
+                var resources = {
+                    "controllers/home@index": {
+                        "javascript": "home-min_1b6316803455be0c0bdbe08fd52c44b5.js",
+                        "css": "home-min_15a63a0c6a738eb8de8e823d2b5aafda.css"
+                    },
+                    "controllers/users@index": {
+                        "javascript": "users-min_f9681fdf76cf085a6c5b2295112398de.js",
+                        "css": "users-min_48d508549dffd696499a5173ef898c13.css"
+                    }
+                };
+                JSON.parse(
+                    fs.readFileSync(__dirname + '/../../rbuild.lock'
+                ).toString()).should.eql(resources);
+            });
         });
     });
 });
