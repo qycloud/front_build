5a6fbb0c83acda87858ce8120b6c0c842f3a91e4
sun <sunweijun1989@gmail.com>
2013-08-28 08:25:30 +0800

    move Gruntfile to root directory

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+    var pkg = grunt.file.readJSON('/tmp/grunt.json');
+    grunt.initConfig({
+        concat: pkg.concat,
+        uglify: pkg.uglify,
+        cssmin: pkg.cssmin
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('build', ['concat', 'uglify', 'cssmin']);
+};
