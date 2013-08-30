16a7f211fc2f8beb8f21d25bf8b8185d934434b3
Tom <hzlhu.dargon@gmail.com>
2013-08-30 09:47:18 +0800

    add task to replace css urls with absolute path

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,28 @@
 module.exports = function(grunt) {
-    var pkg = grunt.file.readJSON(require('os').tmpdir() + '/grunt.json');
-    grunt.initConfig({
-        concat: pkg.concat,
-        uglify: pkg.uglify,
-        cssmin: pkg.cssmin
-    });
+    var pkg = grunt.file.readJSON(require('os').tmpdir() + '/grunt.json'),
+        replaceCssUrl = (typeof pkg.css_url_replace !== 'undefined');
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.registerTask('build', ['concat', 'uglify', 'cssmin']);
+    if (replaceCssUrl) {
+        grunt.initConfig({
+            concat: pkg.concat,
+            uglify: pkg.uglify,
+            css_url_replace: pkg.css_url_replace,
+            cssmin: pkg.cssmin
+        });
+        grunt.loadNpmTasks('grunt-contrib-concat');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-css-url-replace');
+        grunt.loadNpmTasks('grunt-contrib-cssmin');
+        grunt.registerTask('build', ['concat', 'uglify', 'css_url_replace', 'cssmin']);
+    } else {
+        grunt.initConfig({
+            concat: pkg.concat,
+            uglify: pkg.uglify,
+            cssmin: pkg.cssmin
+        });
+        grunt.loadNpmTasks('grunt-contrib-concat');
+        grunt.loadNpmTasks('grunt-contrib-uglify');
+        grunt.loadNpmTasks('grunt-contrib-cssmin');
+        grunt.registerTask('build', ['concat', 'uglify', 'cssmin']);
+    }
 };
