cc16aa3d9f96fda0b6ba04566fc58bd86b8504bc
mtao <jmtao33@gmail.com>
2015-12-09 15:57:28 +0800

    add grunt sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,20 @@
 module.exports = function(grunt) {
-    var pkg = grunt.file.readJSON(require('os').tmpdir() + '/grunt.json'),
-        replaceCssUrl = (typeof pkg.css_url_replace !== 'undefined');
+    var pkg = grunt.file.readJSON(require('os').tmpdir() + '/grunt.json');
 
-    if (replaceCssUrl) {
         grunt.initConfig({
             copy: pkg.copy,
             concat: pkg.concat,
+            sass: pkg.sass,
             uglify: pkg.uglify,
             clean: pkg.clean
         });
         grunt.loadNpmTasks('grunt-contrib-copy');
         grunt.loadNpmTasks('grunt-contrib-uglify');
         grunt.loadNpmTasks('grunt-contrib-concat');
+        grunt.loadNpmTasks('grunt-contrib-sass');
         grunt.loadNpmTasks('grunt-contrib-clean');
         grunt.registerTask(
             'build',
-            ['copy', 'uglify', 'concat', 'clean']
+            ['copy', 'uglify', 'concat', 'sass', 'clean']
         );
-    } else {
-        grunt.initConfig({
-            copy: pkg.copy,
-            concat: pkg.concat,
-            uglify: pkg.uglify,
-            clean: pkg.clean
-        });
-        grunt.loadNpmTasks('grunt-contrib-copy');
-        grunt.loadNpmTasks('grunt-contrib-uglify');
-        grunt.loadNpmTasks('grunt-contrib-concat');
-        grunt.loadNpmTasks('grunt-contrib-clean');
-        grunt.registerTask(
-            'build', ['copy', 'uglify', 'concat', 'clean']
-        );
-    }
 };
